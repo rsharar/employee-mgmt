8bed99b14ac6198ff95208849e552fc4a41f3aa2
Rudy Sharar <rudysharar@D-10-19-92-101.dhcp4.washington.edu>
2018-09-20 19:45:19 -0700

    firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.5.1/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBpKc4gXxVxkmJoSa0Ccb6ONBmGmtOFX4A",
+    authDomain: "employee-data-8b2e4.firebaseapp.com",
+    databaseURL: "https://employee-data-8b2e4.firebaseio.com",
+    projectId: "employee-data-8b2e4",
+    storageBucket: "employee-data-8b2e4.appspot.com",
+    messagingSenderId: "517246618045"
+  };
+  firebase.initializeApp(config);
+
+  
